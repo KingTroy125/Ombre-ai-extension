5f66c7cd7729567f8090b7ccf018ff7727409cd7
Tristan Hendricks <moonknight1355@gmail.com>
2026-07-09 22:17:58 +0200

    feat: bundle extension build assets and update manifest configuration

--- dist/assets/content-script.ts-loader-CPA2Xash.js
+++ dist/assets/content-script.ts-loader-CPA2Xash.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/content-script.ts-BBLXva-2.js")
+      chrome.runtime.getURL("assets/content-script.ts-Dc3KfH0-.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
